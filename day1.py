0fb0cbb6532a0935d81b778bfb5fa9aabcd4a8d9
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-08-22 01:42:47 +0530

    First Commit

---
+++ day1.py
@@ -0,0 +1,13 @@
+import math
+
+# Started Python Day -1
+print("Hello Python!!")
+
+# Area of Circle:
+
+pi = math.pi
+
+radius = int(input("Enter the Value of Radius : "))
+
+area = pi * radius * radius
+print("Area of Given Raduis is : ", area)
