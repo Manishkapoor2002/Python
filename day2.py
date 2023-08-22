4217dd818213773f1292a63760be454c84fd02be
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-08-22 10:18:57 +0530

    added Odd even in if-else

--- day2.py
+++ day2.py
@@ -9,3 +9,11 @@ if(number % 5 == 0):
     print(number , " is divisible by 5")
 else:
     print(number , " is not divisible by 5")
+
+
+# Odd or Even
+checkNum = int(input("Enter Number to check wheter the number is prime or not: "))
+if(checkNum % 2 == 0):
+    print(checkNum , " is Even")
+else:
+    print(checkNum," is odd")
