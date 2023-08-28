021799e309675bf12d2105cfbe4d0b81ac26285c
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-08-28 13:21:36 +0530

    for loop using range.

---
+++ day3.py
@@ -0,0 +1,9 @@
+# for,for-else Loops:
+for i in range(1,11,1): #it will print from 1 to 10(n-1):
+    print("hello World!!",i)
+
+for i in range(2,22,2):
+    print(i)
+
+for i in range(5):
+    print(i)
