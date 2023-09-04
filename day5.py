6c68922e7fa557527ab939b0214ef659bf1d36ca
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-09-04 13:37:07 +0530

    basic of list

---
+++ day5.py
@@ -0,0 +1,10 @@
+list = []
+n =(int(input("Enter the length of array: ")))
+print(n)
+for i in range(n):
+    print("Enter number at Index ", i ," = ",end= " ")
+    a = int(input())
+
+    list.append(a)
+
+print(list)
