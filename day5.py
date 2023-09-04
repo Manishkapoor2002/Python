f667da86f7bfd6bef812f9d7c7c5e06367cac779
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-09-04 13:37:53 +0530

    create a list of power of 2

--- day5.py
+++ day5.py
@@ -1,10 +1,16 @@
-list = []
-n =(int(input("Enter the length of array: ")))
-print(n)
-for i in range(n):
-    print("Enter number at Index ", i ," = ",end= " ")
-    a = int(input())
+import math
+# list = []
+# n =(int(input("Enter the length of array: ")))
+# print(n)
+# for i in range(n):
+#     print("Enter number at Index ", i ," = ",end= " ")
+#     a = int(input())
 
-    list.append(a)
+#     list.append(a)
 
-print(list)
+# print(list)
+powerOfTwo = []
+for i in range(11):
+    powerOfTwo.append(math.pow(2,i))
+
+print(powerOfTwo)
