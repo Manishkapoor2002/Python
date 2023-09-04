b3aacb160b7d60020ccc21923ab68649a413ca98
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-09-04 14:13:13 +0530

    Permutation

--- day5.py
+++ day5.py
@@ -1,4 +1,13 @@
 import math
+# List
+# my_list = [5,6,1,7,3,0,2,8,4]
+# my_list.pop()
+# del my_list[1]
+# my_list.remove(7)
+# print(my_list.index(1))
+# my_list.sort()
+# my_list.reverse()
+# print(my_list)
 # list = []
 # n =(int(input("Enter the length of array: ")))
 # print(n)
@@ -16,5 +25,30 @@ import math
 # print(powerOfTwo)
 
 # comprehension 
-pow = [2 ** x for x in range(11)]
-print(pow)
+# pow = [2 ** x for x in range(11)]
+# print(pow)
+
+# print([x+y for x in ['Python', 'java'] for y in [' Language', ' Programming']])
+
+# membership operators:
+# list = [1,2,3,4,5,6,7,8,9]
+# print(1 in list)
+# print('1' in list)
+
+
+# permutation: 
+n = int(input("Enter the value of n: "))
+r = int(input("Enter the value of r: "))
+factorialOfN = 1
+factorialOfR = 1
+
+for i in range(1,n+1,1):
+    factorialOfN *= i
+    
+
+for i in range(1,(n-r+1),1):
+    factorialOfR *= i 
+
+ncr = factorialOfN/factorialOfR
+
+print("Permutaion of given number : " ,ncr)
