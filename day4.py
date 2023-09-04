920a4e72968b9452d07e39458d70a659f52958d2
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-09-04 13:36:45 +0530

    start list

--- day4.py
+++ day4.py
@@ -1,28 +1,47 @@
 # Jumping Statements:
 # Break statement:
-print("Using Break : ")
+# print("Using Break : ")
 
-for i in range(1,10):
-    if i == 5:
-        break
-    print(i ,end=" ")
+# for i in range(1,10):
+#     if i == 5:
+#         break
+#     print(i ,end=" ")
 
-print()
-print()
+# print()
+# print()
 
-# continue
-print("Using continue : ")
-for i in range(1,10):
-    if i == 5:
-        continue
-    print(i,end=" ")
+# # continue
+# print("Using continue : ")
+# for i in range(1,10):
+#     if i == 5:
+#         continue
+#     print(i,end=" ")
 
-print()
-print()
+# print()
+# print()
 
-# Pass
-print("Using Pass:")
-for i in range(1,10):
-    if i == 5:
-        pass
-    print(i ,end=" ")
+# # Pass
+# print("Using Pass:")
+# for i in range(1,10):
+#     if i == 5:
+#         pass
+#     print(i ,end=" ")
+
+# pass,break,continue:
+
+# for i in range(20):
+#     if i == 5:
+#         continue
+#     elif i == 10:
+#         pass
+#     elif i == 15:
+        # break
+#     print(i)
+
+    # List 
+    # empty list 
+my_List = [1,2,3,4,5,6,7,8,9]
+
+for i in range(10):
+    print(my_List[i])
+    
