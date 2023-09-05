7c48d958f756956cd806af8c08f58ec89f1ae2b7
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-09-05 10:47:58 +0530

    more in sets

--- day6.py
+++ day6.py
@@ -13,12 +13,18 @@
 
 # Sets:(No duplicate item, unordered)
 
-Set = {1,2,3,'one','two','three',3,2,1,'one'}
-print(Set)
-Set.add('four')
-print(Set)
-Set.remove('four')
-print(Set)
-print(id(Set))
+# Set = {1,2,3,'one','two','three',3,2,1,'one'}
+# print(Set)
+# Set.add('four')
+# print(Set)
+# Set.remove('four')
+# print(Set)
+# print(id(Set))
+
+set1 = {1,2,4,6,8}
+set2 = {2,5,8,0,2,7}
+print(set1.union(set2))
+print(set1.intersection(set2))
+print(set1.difference(set2))
 
 
