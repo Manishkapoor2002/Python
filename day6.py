f9f06f849dd5b959580dd5e412aef30daea77240
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-09-05 10:27:04 +0530

    more into tuple

--- day6.py
+++ day6.py
@@ -1,5 +1,12 @@
 # Tuple:
-tuple = (2,3,5,4,8,9,6)
-print(tuple)
-manish = 123,
-print(type(manish))
+# tuple = (2,3,5,4,8,9,6)
+# print(id(tuple))
+# manish = 123,
+# print(type(manish))
+
+tuple = (7,8,5,2,3,6)
+print(id(tuple))
+
+arr = [1,2,3,4,5]
+print(id(arr))
+
