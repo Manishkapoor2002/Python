eecef4a0e75c01b6b63921398314142c35b87347
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-09-05 10:40:16 +0530

    basic of sets

--- day6.py
+++ day6.py
@@ -4,9 +4,21 @@
 # manish = 123,
 # print(type(manish))
 
-tuple = (7,8,5,2,3,6)
-print(id(tuple))
+# tuple = (7,8,5,2,3,6)
+# print(id(tuple))
 
-arr = [1,2,3,4,5]
-print(id(arr))
+# arr = [1,2,3,4,5]
+# print(id(arr))
+
+
+# Sets:(No duplicate item, unordered)
+
+Set = {1,2,3,'one','two','three',3,2,1,'one'}
+print(Set)
+Set.add('four')
+print(Set)
+Set.remove('four')
+print(Set)
+print(id(Set))
+
 
