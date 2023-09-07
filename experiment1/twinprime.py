32562852332ce86ca6c65aca28991e4d044b3092
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-09-07 22:59:10 +0530

    twinprime

---
+++ experiment1/twinprime.py
@@ -0,0 +1,19 @@
+temp = 2
+count = 0
+for i in range(3,1000):
+    for j in range(2,i):
+        if(i%j==0):
+            break
+    else:
+        if((i - temp )== 2):
+            print("(",end="")
+            print(temp,",",i,end="")
+            print("), ",end="")
+
+            count = count + 1
+
+        # print("Number is Prime : ", i)
+        temp = i
+print(count)
+
+        
