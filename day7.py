10b8011eae33d482cd66f3c6c7177b161e591db4
MANISH KAPOOR <128586718+Manishkapoor2002@users.noreply.github.com>
2023-09-11 13:32:07 +0530

    basics of functions

---
+++ day7.py
@@ -0,0 +1,13 @@
+def manish(max1,max2):
+    if(max1<max2):
+        result = max2
+    else:
+        result = max2
+    return result
+
+
+
+max1 = int(input("Enter number : "))
+max2 = int(input("Enter number : "))
+
+print(manish(max1,max2))
